c4832ef6e261351b37aa594cc3a56845a4958985
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2024-02-09 15:39:50 +0600

    added admin and login panel

--- aitechland/safety_detection/models.py
+++ aitechland/safety_detection/models.py
@@ -4,36 +4,46 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     # Add any additional user profile information here, if needed
+    class Meta:
+        db_table = 'user'
+        managed = False
+
+
+class DetectionClasses(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'detection_class'
+        managed = False
 
 
 class Camera(models.Model):
-    name = models.CharField(max_length=100)
+    detect_name = models.ForeignKey(DetectionClasses, on_delete=models.CASCADE)
     ip_address = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # Add any additional camera information here
+
+    class Meta:
+        db_table = 'camera'
+        managed = False
 
 
 class Permission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+
     # Add any additional permission information here, like roles
+    class Meta:
+        db_table = 'permission'
+        managed = False
 
 
 class Image(models.Model):
     camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
     class_name = models.CharField(max_length=100)
     image_file = models.ImageField(upload_to='images/')
-    # Add any additional image-related information here, like timestamp
 
-
-class DetectionClasses(models.Model):
-    name = models.CharField(max_length=100)
-    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
-    # Add any additional class name information here
-
-
-class test(models.Model):
-    name = models.CharField(max_length=100)
-    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
-    # Add any additional class name information here
+    class Meta:
+        db_table = 'image'
+        managed = False
