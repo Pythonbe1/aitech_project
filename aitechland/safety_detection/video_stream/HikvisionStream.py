f9ebc1bc0400d61bdfbcccc770513ecb654f8fc7
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2024-02-19 21:10:48 +0600

    added streaming

--- aitechland/safety_detection/video_stream/HikvisionStream.py
+++ aitechland/safety_detection/video_stream/HikvisionStream.py
@@ -1,31 +1,25 @@
+import threading
+
 import cv2
-import numpy as np
-from hikvisionapi import Client
 
-class VideoStream:
-    def __init__(self, ip_address, username, password):
-        self.ip_address = ip_address
-        self.username = username
-        self.password = password
 
-    def start_video_stream(self):
-        # Connect to the IP camera
-        cam = Client(self.ip_address, self.username, self.password)
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
 
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
         while True:
-            # Capture video frame
-            vid = cam.Streaming.channels[102].picture(method='get', type='opaque_data')
+            (self.grabbed, self.frame) = self.video.read()
 
-            # Decode and yield frames continuously
-            bytes = b''
-            for chunk in vid.iter_content(chunk_size=1024):
-                bytes += chunk
-                a = bytes.find(b'\xff\xd8')
-                b = bytes.find(b'\xff\xd9')
-                if a != -1 and b != -1:
-                    jpg = bytes[a:b + 2]
-                    bytes = bytes[b + 2:]
-                    frame = cv2.imdecode(
-                        np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                    _, jpeg = cv2.imencode('.jpg', frame)
-                    yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+
+
--- aitechland/safety_detection/views.py
+++ aitechland/safety_detection/views.py
@@ -1,33 +1,31 @@
+import cv2
 from django.http import StreamingHttpResponse
 from django.views.generic import TemplateView
 from safety_detection.models import Permission, Camera
-from safety_detection.video_stream.HikvisionStream import VideoStream
 
 
 class VideoStreamView(TemplateView):
     template_name = 'safety_detection/main.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def render_to_response(self, context, **response_kwargs):
         logged_in_user = self.request.user
         camera_ids = Permission.objects.filter(user_id=logged_in_user.id).values_list('camera_id', flat=True)
         ip_cameras = Camera.objects.filter(id__in=camera_ids).values_list('ip_address', flat=True)
-        context['ip_camera_urls'] = ['http://' + ip for ip in ip_cameras]
-        return context
 
-    def get(self, request, *args, **kwargs):
-        ip_addresses = self.get_context_data().get('ip_camera_urls')
-        username = 'admin'
-        password = '12345678a'
+        # Create a list to store VideoCapture objects for each camera
+        videos = [cv2.VideoCapture(f"rtsp:/admin:12345678a@{ip}:554/Streaming/Channels/1") for ip in ip_cameras]
 
-        video_streams = []
+        def generate_frames():
+            while True:
+                for video in videos:
+                    success, frame = video.read()
+                    if not success:
+                        break
+                    _, buffer = cv2.imencode('.jpg', frame)
+                    frame_bytes = buffer.tobytes()
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
-        if ip_addresses:
-            # Create a VideoStream instance for each camera
-            for ip_address in ip_addresses:
-                video_stream = VideoStream(ip_address, username, password)
-                video_streams.append(video_stream)
+        return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
 
-        # Start streaming from all cameras
-        merged_stream = zip(*[video_stream.start_video_stream() for video_stream in video_streams])
-        return StreamingHttpResponse(merged_stream, content_type='multipart/x-mixed-replace; boundary=frame')
+
