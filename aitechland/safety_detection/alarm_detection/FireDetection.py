7975a8840f7953a8353041e74c36e2a5a35fb3b2
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2024-04-11 16:32:00 +0500

    added style

--- aitechland/safety_detection/alarm_detection/FireDetection.py
+++ aitechland/safety_detection/alarm_detection/FireDetection.py
@@ -5,6 +5,6 @@ class FireDetection:
     @staticmethod
     def get_fire_detection(path_weights, frame):
         model = YOLO(path_weights)
-        results = model(frame)
+        results = model.predict(frame, conf=0.2)
         annotated_frame = results[0].plot()
         return annotated_frame
