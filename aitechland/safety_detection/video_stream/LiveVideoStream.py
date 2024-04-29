0882476fd46105bbf8ea5a128c0bd062c6ce8b44
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2024-04-29 15:04:59 +0500

    added style

--- aitechland/safety_detection/video_stream/LiveVideoStream.py
+++ aitechland/safety_detection/video_stream/LiveVideoStream.py
@@ -18,6 +18,7 @@ class VideoCamera(object):
         success, imgNp = self.url.read()
         resize = cv2.resize(imgNp, (640, 480), interpolation=cv2.INTER_LINEAR)
         ret, jpeg = cv2.imencode('.jpg', resize)
+
         return resize
 
     @staticmethod
