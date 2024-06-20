7cb4c08d5f207792ad96f3345d6e24422f390d31
Pythonbe1 <bekbol.sultankulov2@gmail.com>
2024-06-20 15:44:33 +0500

    added analysis.html view and modified cameraStream class

--- aitechland/safety_detection/urls.py
+++ aitechland/safety_detection/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('alarm_index/', views.alarm_index, name='alarm_index'),
     path('alarm_index/<str:filter_param>', views.alarm_index, name='alarm_index'),
     path('alarm_index_export/', views.alarm_index_export, name='alarm_index_export'),
-    path('monitoring_index/', views.monitoring_index, name='monitoring_index')
+    path('monitoring_index/', views.monitoring_index, name='monitoring_index'),
+    path('analysis/', views.analysis, name='analysis'),
 ]
